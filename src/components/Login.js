4442f3984597a1007eca2012a3549038df10a796
Brittany <sportswomen@hotmail.com>
2022-06-01 00:44:35 +1200

    Final visual adjustments for login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,15 +10,15 @@ export const Login = () =>{
             <Navbar></Navbar>
             <div className="image"></div>
                 <div className="base-container">
-                    <div className="header1">LOG IN</div>
+                    <div className="header1">LOG IN:</div>
                     <div className="content"></div>
                     <div className="form1">
                         <div className="form-group">
-                            <label htmlFor="username">Username</label>
+                            <label htmlFor="username">Username:</label>
                             <input type="text" name="username" placeholder="username"/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="password">Password</label>
+                            <label htmlFor="password">Password:</label>
                             <input type="password" name="password" placeholder="password"/>
                         </div>
                     </div>
