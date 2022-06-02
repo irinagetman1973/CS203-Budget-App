70aece02a84faaa96934f5bdf8e39dad60983599
Brittany <sportswomen@hotmail.com>
2022-06-03 08:40:52 +1200

    fixed merging issue

--- src/components/DashboardPageLayout.js
+++ src/components/DashboardPageLayout.js
@@ -16,6 +16,7 @@ export const DashboardPageLayout = () => {
     const navigateToExpenses = () => {
         navigate('/Expenses');
     };
+
     return (
         <>
             <div className="db-layout">
@@ -51,43 +52,20 @@ export const DashboardPageLayout = () => {
                                 <h3>
                                     Expenses
                                 </h3>
-                                {/*TEMPORARY*/} 
-                                
+                                {/*TEMPORARY*/}
+
                                 <div className="budget-box">
                                     {/* <div className="button">*/}
-                                        <button className='sign-btn' onClick={navigateToExpenses}>
-                                            Expenses
-                                        </button>
-                                        <Routes>
-                                            <Route path="/Expenses" element={<Expenses />} />
-                                        </Routes>
+                                    <button className='sign-btn' onClick={navigateToExpenses}>
+                                        Expenses
+                                    </button>
+                                    <Routes>
+                                        <Route path="/Expenses" element={<Expenses />} />
+                                    </Routes>
                                     {/* </div> */}
 
                                 </div>
 
-                        <p>
-                            Here is your own space
-                        </p>
-                        <button className="editProfile-btn">
-                            Edit profile
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="budget-section">
-                <div className="budget-section-wrap">
-                    <h2>
-                        Choose your tool 
-           
-                    </h2>
-                    <div className="budget-items">
-                        <div className="link-expenses">
-                            <h3>
-                                Expenses
-                            </h3>
-                            <div className="budget-box">
-                            
-                            
                             </div>
                             <div className="link-income">
                                 <h3>
