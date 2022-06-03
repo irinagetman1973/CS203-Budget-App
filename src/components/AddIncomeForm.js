20c5a54780de2cd236c3181463bc154af28b87f3
Brittany <sportswomen@hotmail.com>
2022-06-03 22:40:12 +1200

    Reformatted the income pages

--- src/components/AddIncomeForm.js
+++ src/components/AddIncomeForm.js
@@ -1,42 +1,42 @@
 import React from 'react'
 
-const AddIncomeForm = () => {
-  return (
-      <form>
-        <div className='row'>
-            <div className='col-sm'>
-                <label for='source' >
-                    Source
-                </label>
-                <input 
-                    required ='required' 
-                    type='text' 
-                    className='form-control' 
-                    id='source'
-                 ></input>
+export const AddIncomeForm = () => {
+    return (
+        <form>
+            <div className='row'>
+                <div className='col-sm'>
+                    <label for='source' >
+                        Source
+                    </label>
+                    <input
+                        required='required'
+                        type='text'
+                        className='form-control'
+                        id='source'
+                    ></input>
 
-            </div>
-            <div className='col-sm'>
-                <label for='amount'>
-                    Amount
-                </label>
-                <input 
-                required='required'
-                type='text'
-                className='form-control'
-                id='amount'
-                ></input>
+                </div>
+                <div className='col-sm'>
+                    <label for='amount'>
+                        Amount
+                    </label>
+                    <input
+                        required='required'
+                        type='text'
+                        className='form-control'
+                        id='amount'
+                    ></input>
 
-            </div>
-            <div className='col-sm'>
-                <button 
-                type='submit'
-                className='btn btn-primary'>Save</button>
+                </div>
+                <div className='col-sm'>
+                    <button
+                        type='submit'
+                        className='btn btn-primary'>Save</button>
 
+                </div>
             </div>
-        </div>  
-      </form>
-  )
+        </form>
+    )
 }
 
 export default AddIncomeForm
