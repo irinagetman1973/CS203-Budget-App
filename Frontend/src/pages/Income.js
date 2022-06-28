617a84b539c43cf7ad1ac13afbdb7b440c22f39e
irinagetman1973 <84743369+irinagetman1973@users.noreply.github.com>
2022-06-29 11:52:33 +1200

    Changed Income.js (all jsx in)

--- Frontend/src/pages/Income.js
+++ Frontend/src/pages/Income.js
@@ -2,20 +2,49 @@ import React from 'react';
 
 import '../App.css'
 import { Navbar } from '../components/Navbar';
-import { Income } from '../components/Income'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-function Income_() {
+import IncomeList from '../components/IncomeList';
+import AddIncomeForm from '../components/AddIncomeForm';
+import { AppProvider } from '../components/context/AppContext';
+
+export const Income = () => {
     return (
         <>
             <Navbar />
-            <Income />
+            <AppProvider>
+                    <div className='container'>
+                        {/*  */}
+                        <h1 className='mt-3'>Your Income</h1>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                {/* <Budget /> */}
+                            </div>
+                        </div>
+                        <h3 className='mt-3'>
+                            Income
+                        </h3>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <IncomeList />
+                            </div>
+                        </div >
+                        <h3 className='mt-3'>
+                            Add Income
+                        </h3>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AddIncomeForm />
+                            </div>
+
+                        </div>
+
+                    </div>
+            </AppProvider>
         </>
     )
 }
 
 export default Income
-
-//Irina
-
-{/*Fixed by Brittany*/ }
